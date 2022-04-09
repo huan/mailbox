b5b9cdf151cedac293a703c4c21b6fb9c43917b6
Huan (李卓桓) <zixia@zixia.net>
2022-04-09 17:55:06 +0800

    clean

--- src/is/is-address.ts
+++ src/is/is-address.ts
@@ -2,8 +2,8 @@ import type { Address }  from '../impls/address-interface.js'
 
 export function isAddress (value: any): value is Address {
   return !!value
-    && typeof value.id === 'string'
-    && typeof value.send === 'function'
-    && typeof value.toString === 'function'
-    && value.toString() === value.id
+   && typeof value.send === 'function'
+     && typeof value.toString === 'function'
+        && typeof value.id === 'string'
+          && value.toString() === value.id
 }
