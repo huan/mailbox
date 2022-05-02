b5f94243b2554a0f0a51c144a042e9091d1ba0ed
Huan (李卓桓) <zixia@zixia.net>
2022-05-02 22:55:54 +0800

    add snapshot unit test

--- src/context/child/snapshot.spec.ts
+++ src/context/child/snapshot.spec.ts
@@ -1,10 +1,61 @@
 #!/usr/bin/env -S node --no-warnings --loader ts-node/esm
 /* eslint-disable sort-keys */
 
-import { test }    from 'tstest'
+import { test }                     from 'tstest'
+import { createMachine, actions, interpret, AnyEventObject }   from 'xstate'
 
 import { snapshot }   from './snapshot.js'
 
 test('snapshot() smoke testing', async t => {
-  t.ok(snapshot, 'tbw')
+  const CHILD_ID = 'child-id'
+  const child = createMachine({
+    id: CHILD_ID,
+    context: {
+      child: true,
+    },
+    initial: 'idle',
+    states: {
+      idle: {},
+    },
+  })
+
+  const PARENT_ID = 'parent-id'
+  const parent = createMachine<any, any>({
+    id: PARENT_ID,
+    invoke: {
+      id: CHILD_ID,
+      src: child,
+    },
+    initial: 'idle',
+    states: {
+      idle: {
+        on: {
+          TEST: {
+            actions: actions.choose<any, any>([
+              {
+                cond: (_, __, meta) => !!snapshot(CHILD_ID)(meta.state),
+                actions: actions.send('SNAPSHOT_GOT'),
+              },
+            ]),
+          },
+        },
+      },
+    },
+  })
+
+  const eventList: AnyEventObject[] = []
+  const interpreter = interpret(parent)
+    .onEvent(e => {
+      eventList.push(e)
+    })
+    .start()
+
+  eventList.length = 0
+  interpreter.send('TEST')
+  await new Promise(setImmediate)
+
+  t.same(eventList, [
+    { type: 'TEST' },
+    { type: 'SNAPSHOT_GOT' },
+  ], 'should be able to get snapshot of CHILD_ID in actions.choose of PARENT_ID')
 })
